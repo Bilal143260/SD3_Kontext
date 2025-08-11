1f03d60787a0a1b87961a27b2d4cfaa9f243d1e1
sahalshah42@gmail.com <sahalshah42@gmail.com>
2025-08-11 14:00:24 +0500

    add train class

--- train.py
+++ train.py
@@ -483,23 +483,18 @@ def main(args):
             safeguard_warmup=args.prodigy_safeguard_warmup,
         )
 
-    # Dataset and DataLoaders creation:
-    train_dataset = SD3KontextDataset(
-        instance_data_root=args.instance_data_dir,
-        instance_prompt=args.instance_prompt,
-        class_prompt=args.class_prompt,
-        class_data_root=args.class_data_dir if args.with_prior_preservation else None,
-        class_num=args.num_class_images,
-        size=args.resolution,
-        repeats=args.repeats,
-        center_crop=args.center_crop,
+    train_dataset = KontextDataset(
+        dataset_name=args.dataset_name,
+        source_column_name=args.source_column,
+        target_column_name=args.target_column,
+        caption_column_name=args.caption_column,
+        size=(args.width, args.height),
+        split="train",  # Training split
     )
 
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
-        batch_size=args.train_batch_size,
-        shuffle=True,
-        collate_fn=lambda examples: collate_fn(examples, args.with_prior_preservation),
+        collate_fn=collate_fn,
         num_workers=args.dataloader_num_workers,
     )
 
